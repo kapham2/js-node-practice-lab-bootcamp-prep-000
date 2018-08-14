465323c881e9e820f223327c3804b9291170eec6
Kim Pham <pham.kim.a@gmail.com>
2018-08-14 03:40:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
 var name = "Kim"
-var height = "60"
+var height = 60
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
-console.log(parseInt(height)+1)
+console.log(height+1)
